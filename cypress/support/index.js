bfc96ee39b3314a1fce54b840f69164b4cd1878f
Douglas Fugazi <itmteleco@gmail.com>
2023-09-17 18:52:24 -0500

    New update to this test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,14 +16,7 @@
 // Import commands.js using ES2015 syntax:
 
 import '@applitools/eyes-cypress/commands'
-
 import './commands'
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-  })
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
